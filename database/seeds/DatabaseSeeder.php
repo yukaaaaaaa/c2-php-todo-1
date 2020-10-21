1e7f37e7bad6bb806b6257a61f279f8b3a145cf7
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-10-21 11:44:14 +0900

    fa

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +10,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TodoTableSeeder::class);
+         // TODO: UserSeederをTodoTableSeederに変更する 
+         // $this->call(UserSeeder::class);
+         $this->call(TodoTableSeeder::class);
     }
 }
