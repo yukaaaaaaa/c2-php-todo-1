83552943017940b465bbfdd0b4adc0f407de6d10
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-10-28 11:36:35 +0900

    ログインできるようになった。

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::resource('todo', 'TodoController', ['only' => [
-    'index', 'show'
-]]);
+Auth::routes();
+
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::resource('todo', 'TodoController', ['only' => [
+        'index', 'show'
+    ]]);
+});
