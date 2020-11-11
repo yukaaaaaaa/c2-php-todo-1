c4b0a325e608a1457612b21cd5ac040fc2abaf28
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-11 12:29:51 +0900

    hennkou

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -41,7 +41,14 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $todo new Todo();
+        $todo->title = $request->title;
+        $todo->due_date =$request->due_date;
+        $todo->statud = Todo::STATUS_NOT_YET;
+
+        Auth::user()
+
+        
     }
 
     /**
